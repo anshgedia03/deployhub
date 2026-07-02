d3d84efbc268618b739e01091c57770ac34e999f
anshgedia03 <anshg@zignuts.com>
2026-07-02 14:32:54 +0530

    public_host_new_ip

--- shared/src/services/nginx.service.ts
+++ shared/src/services/nginx.service.ts
@@ -40,6 +40,9 @@ server {
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto $scheme;
         proxy_cache_bypass $http_upgrade;
+        
+        # Strip Secure flag from cookies since we serve over HTTP
+        proxy_cookie_flags ~ secure off;
     }
 }
 `;
